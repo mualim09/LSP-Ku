7616cabbb896d0cd18d6dbb19c06ca40063aee00
Tera Byte <tubagus.aom.swk@gmail.com>
2022-10-14 10:04:07 +0700

    mergehp

--- application/views/templates/bootstraps/pengurus.php
+++ application/views/templates/bootstraps/pengurus.php
@@ -141,67 +141,6 @@
           <div class="box-shdw-tb">
             <span class="thumb-info custom-thumb-info-style-2" style="border-radius: 4px;">
               <span class="thumb-info-wrapper m-none">
-                <a href="#team-content-3" class="popup-with-zoom-anim text-decoration-none" title="Jeff Damaris Pribadi">
-                  <img src="<?=base_url()?>assets/img/pengurus/jeff.jpg" class="img-responsive" alt="">
-                </a>
-              </span>
-              <span class="thumb-info-caption background-color-dark-tb center p-md">
-                <h4 style="font-size:0.8em!important;">
-                  <a href="#team-content-3" class="popup-with-zoom-anim text-decoration-none text-color-light" title="JEFF DAMARIS PRIBADI">
-                    JEFF DAMARIS PRIBADI
-                  </a>
-                </h4>
-                <p class="custom-thumb-info-category text-color-light">DIREKTUR UTAMA</p>
-              </span>
-            </span>
-            <div id="team-content-3" class="dialog dialog-lg zoom-anim-dialog mfp-hide p-xlg">
-              <div class="row">
-                <div class="col-md-4 col-sm-4">
-                  <img src="<?=base_url()?>assets/img/pengurus/jeff.jpg" class="custom-rounded-image img-responsive mb-lg mt-xlg" alt="">
-                  <ul class="social-icons custom-social-icons-style-2 center">
-                    <li class="social-icons-facebook">
-                      <a href="http://www.facebook.com/" target="_blank" title="Facebook">
-                        <i class="fa fa-facebook"></i>
-                      </a>
-                    </li>
-                    <li class="social-icons-twitter">
-                      <a href="http://www.twitter.com/" target="_blank" title="Twitter">
-                        <i class="fa fa-twitter"></i>
-                      </a>
-                    </li>
-                    <li class="social-icons-google">
-                      <a href="http://www.plus.google.com/" target="_blank" title="Twitter">
-                        <i class="fa fa-google-plus" aria-hidden="true"></i>
-                      </a>
-                    </li>
-                    <li class="social-icons-linkedin">
-                      <a href="http://www.linkedin.com/" target="_blank" title="Linkedin">
-                        <i class="fa fa-linkedin"></i>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                <div class="col-md-8 col-sm-8">
-                  <h2 class="font-weight-semibold text-color-dark">Jeff Damaris Pribadi</h2>
-                  <p class="font-weight-semibold">Direktur Utama</p>
-
-                  <p class="font-size-sm">Bapak Jeff Damaris Pribadi adalah Warga Negara Indonesia usia 33 tahun (7 Maret 1989). Lulusan ADAMS University, Colorado, tahun 2009. Beberapa pengalaman dan jabatan profesional:</p>
-
-                  <ol style="padding-left:15px;">
-                    <li>Direktur di PT Abadi Berkat Perkasa (2014 - 2019).</li>
-                    <li>Komisaris di PT Lintas Prima Perkasa (2019 – sekarang).</li>
-                    <li>Direktur Utama di PT Pacific Mitra Sarana (2019 – sekarang).</li>
-                    <li>Direktur Utama di PT Lintas Maritim Jaya (2019 – sekarang).</li>
-                  </ol>
-
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="box-shdw-tb">
-            <span class="thumb-info custom-thumb-info-style-2" style="border-radius: 4px;">
-              <span class="thumb-info-wrapper m-none">
                 <a href="#team-content-4" class="popup-with-zoom-anim text-decoration-none" title="Alexander Susanto">
                   <img src="<?=base_url()?>assets/img/pengurus/alex.jpg" class="img-responsive" alt="">
                 </a>
@@ -212,7 +151,7 @@
                     ALEXANDER SUSANTO
                   </a>
                 </h4>
-                <p class="custom-thumb-info-category text-color-light">DIREKTUR</p>
+                <p class="custom-thumb-info-category text-color-light">DIREKTUR UTAMA</p>
               </span>
             </span>
             <div id="team-content-4" class="dialog dialog-lg zoom-anim-dialog mfp-hide p-xlg">
@@ -244,7 +183,7 @@
                 </div>
                 <div class="col-md-8 col-sm-8">
                   <h2 class="font-weight-semibold text-color-dark">Alexander Susanto</h2>
-                  <p class="font-weight-semibold">Direktur</p>
+                  <p class="font-weight-semibold">Direktur Utama</p>
 
                   <p class="font-size-sm">Bapak Alexander Susanto adalah Warga Negara Indonesia usia 46 tahun (07 November 1975). Mengenyam pendidikan di Universitas Taruma Negara angkatan 1993. Beberapa pengalaman dan jabatan profesional:</p>
 
@@ -254,6 +193,67 @@
                     <li>Direktur di PT Lintas Prima Perkasa (2019 – sekarang).</li>
                     <li>Direktur di PT Pacific Mitra Sarana (2019 – sekarang).</li>
                     <li>Direktur di PT Lintas Maritim Jaya (2019 – sekarang).</li>
+                  </ol>
+
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="box-shdw-tb">
+            <span class="thumb-info custom-thumb-info-style-2" style="border-radius: 4px;">
+              <span class="thumb-info-wrapper m-none">
+                <a href="#team-content-3" class="popup-with-zoom-anim text-decoration-none" title="Jeff Damaris Pribadi">
+                  <img src="<?=base_url()?>assets/img/pengurus/jeff.jpg" class="img-responsive" alt="">
+                </a>
+              </span>
+              <span class="thumb-info-caption background-color-dark-tb center p-md">
+                <h4 style="font-size:0.8em!important;">
+                  <a href="#team-content-3" class="popup-with-zoom-anim text-decoration-none text-color-light" title="JEFF DAMARIS PRIBADI">
+                    JEFF DAMARIS PRIBADI
+                  </a>
+                </h4>
+                <p class="custom-thumb-info-category text-color-light">DIREKTUR</p>
+              </span>
+            </span>
+            <div id="team-content-3" class="dialog dialog-lg zoom-anim-dialog mfp-hide p-xlg">
+              <div class="row">
+                <div class="col-md-4 col-sm-4">
+                  <img src="<?=base_url()?>assets/img/pengurus/jeff.jpg" class="custom-rounded-image img-responsive mb-lg mt-xlg" alt="">
+                  <ul class="social-icons custom-social-icons-style-2 center">
+                    <li class="social-icons-facebook">
+                      <a href="http://www.facebook.com/" target="_blank" title="Facebook">
+                        <i class="fa fa-facebook"></i>
+                      </a>
+                    </li>
+                    <li class="social-icons-twitter">
+                      <a href="http://www.twitter.com/" target="_blank" title="Twitter">
+                        <i class="fa fa-twitter"></i>
+                      </a>
+                    </li>
+                    <li class="social-icons-google">
+                      <a href="http://www.plus.google.com/" target="_blank" title="Twitter">
+                        <i class="fa fa-google-plus" aria-hidden="true"></i>
+                      </a>
+                    </li>
+                    <li class="social-icons-linkedin">
+                      <a href="http://www.linkedin.com/" target="_blank" title="Linkedin">
+                        <i class="fa fa-linkedin"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div class="col-md-8 col-sm-8">
+                  <h2 class="font-weight-semibold text-color-dark">Jeff Damaris Pribadi</h2>
+                  <p class="font-weight-semibold">Direktur</p>
+
+                  <p class="font-size-sm">Bapak Jeff Damaris Pribadi adalah Warga Negara Indonesia usia 33 tahun (7 Maret 1989). Lulusan ADAMS University, Colorado, tahun 2009. Beberapa pengalaman dan jabatan profesional:</p>
+
+                  <ol style="padding-left:15px;">
+                    <li>Direktur di PT Abadi Berkat Perkasa (2014 - 2019).</li>
+                    <li>Komisaris di PT Lintas Prima Perkasa (2019 – sekarang).</li>
+                    <li>Direktur Utama di PT Pacific Mitra Sarana (2019 – sekarang).</li>
+                    <li>Direktur Utama di PT Lintas Maritim Jaya (2019 – sekarang).</li>
                   </ol>
 
                 </div>
